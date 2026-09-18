773f573acf92fc8db4248a7e04fd2ca8325d1e03
unknown <huzaifa.tanveer@dexnive.com>
2026-09-18 07:27:49 -0700

    image blur

--- src/app/my-jobs/[id]/page.tsx
+++ src/app/my-jobs/[id]/page.tsx
@@ -34,6 +34,7 @@ export default function MyJobDetailPage() {
           backHref="/my-jobs"
           showPurchaseButton={false}
           showHeaderStatus
+          blurImage
         />
       )}
     </MainAppShell>
